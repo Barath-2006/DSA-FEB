31ac8d8aee401273755502a6102b76a968643a7c
Barath-2006 <lbarathraj2006@gmail.com>
2026-02-05 19:23:20 +0530

    valid paranthesis

---
+++ valid_paranthesis.java
@@ -0,0 +1,22 @@
+class valid_paranthesis {
+    public boolean isValid(String s) {
+        Stack<Character> stack =new Stack<>();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char c=stack.pop();
+                if(s.charAt(i)==')' && c!='(' || s.charAt(i)=='}' && c!='{' || s.charAt(i)==']' && c!='['){
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
--- valid_square.java
+++ valid_square.java
@@ -1,10 +1,10 @@
 class Solution {
-    public boolean isPerfectSquare(int num) {
-        if(Math.pow(num,0.5)%1==0){
-            return true;
-        }
-        else{
-            return false;
+    public static void main(String[] args) {
+        int i=0;
+        while(i<3);
+        {
+           i++;
+           System.out.print(i); 
         }
     }
 }
